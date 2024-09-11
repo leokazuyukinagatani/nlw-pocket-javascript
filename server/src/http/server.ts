60bb5c59e9e5f4778b6cacbfc3858a75826b6656
leokazuyukinagatani <lknlkn15@hotmail.com>
2024-09-10 21:49:49 -0300

    feat: rota summary

--- server/src/http/server.ts
+++ server/src/http/server.ts
@@ -7,6 +7,7 @@ import {
 import { createGoalRoute } from "./routes/create-goal";
 import { createCompletionRoute } from "./routes/create-completion";
 import { getPendingGoalsRoute } from "./routes/get-pending-goals";
+import { getWeekSummaryRoute } from "./routes/get-week-summary";
 import fastifyCors from "@fastify/cors";
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
@@ -21,6 +22,7 @@ app.setSerializerCompiler(serializerCompiler);
 app.register(createGoalRoute);
 app.register(createCompletionRoute);
 app.register(getPendingGoalsRoute);
+app.register(getWeekSummaryRoute);
 
 app
   .listen({
